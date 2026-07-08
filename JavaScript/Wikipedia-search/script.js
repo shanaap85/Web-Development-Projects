baac5a32d07eb4bc15d9c8e824b8afb15d4560bf
Fathimath Shana AP <shanaap85@gmail.com>
2026-07-08 15:45:52 +0530

    Refactor Wikipedia search functionality

--- JavaScript/Wikipedia-search/script.js
+++ JavaScript/Wikipedia-search/script.js
@@ -1,34 +1,70 @@
-let seI = document.getElementById("searchInput");
-let seR = document.getElementById("searchResults");
-let option = {
-    method: "GET",
+
+let searchInputEl = document.getElementById("searchInput");
+let searchResultsEl = document.getElementById("searchResults");
+let spinnerEl = document.getElementById("spinner");
+
+function createAndAppendSearchResult(result) {
+    let { link, title, description } = result;
+
+    let resultItemEl = document.createElement("div");
+    resultItemEl.classList.add("result-item");
+
+    let titleEl = document.createElement("a");
+    titleEl.href = link;
+    titleEl.target = "_blank";
+    titleEl.textContent = title;
+    titleEl.classList.add("result-title");
+    resultItemEl.appendChild(titleEl);
+
+    let titleBreakEl = document.createElement("br");
+    resultItemEl.appendChild(titleBreakEl);
+
+    let urlEl = document.createElement("a");
+    urlEl.classList.add("result-url");
+    urlEl.href = link;
+    urlEl.target = "_blank";
+    urlEl.textContent = link;
+    resultItemEl.appendChild(urlEl);
+
+    let linkBreakEl = document.createElement("br");
+    resultItemEl.appendChild(linkBreakEl);
+
+    let descriptionEl = document.createElement("p");
+    descriptionEl.classList.add("link-description");
+    descriptionEl.textContent = description;
+    resultItemEl.appendChild(descriptionEl);
+
+    searchResultsEl.appendChild(resultItemEl);
 }
-let el;
-seI.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        el = seI.value;
+
+function displayResults(searchResults) {
+    spinnerEl.classList.add("d-none");
+
+    for (let result of searchResults) {
+        createAndAppendSearchResult(result);
     }
-    let url = "https://apis.ccbp.in/wiki-search?search=" + el;
-    fetch(url, option)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(jsonData) {
-            let a = jsonData.search_results;
-            for (let i of a) {
-                console.log(i)
-                let div = document.createElement("div");
-                let title = document.createElement("h1");
-                title.textContent = i.title;
-                div.appendChild(title);
-                let link = document.createElement("a");
-                link.textContent = i.link;
-                link.setAttribute("href", i.link);
-                div.appendChild(link);
-                let des = document.createElement("p");
-                des.textContent = i.description;
-                div.appendChild(des);
-                seR.appendChild(div);
-            }
-        })
-})
+}
+
+function searchWikipedia(event) {
+    if (event.key === "Enter") {
+
+        spinnerEl.classList.remove("d-none");
+        searchResultsEl.textContent = "";
+
+        let searchInput = searchInputEl.value;
+        let url = "https://ccbp.in" + searchInput;
+        let options = {
+            method: "GET"
+        };
+
+        fetch(url, options)
+            .then(function (response) {
+                return response.json();
+            });
+
+            .then(function (jsonData) {
+                let { search_results } = jsonData;
+                displayResults(search_results);
+            });
+    }
+}
